5aa9c613b5001a6f9cd77c28e0bd352b4e891375
Nicolae Serban <nicolae.serban@techquarter.io>
2026-06-16 11:02:58 +0300

    Added Database migration

--- MoviesWatchListAPI/Data/AppDbContext.cs
+++ MoviesWatchListAPI/Data/AppDbContext.cs
@@ -1,6 +1,29 @@
-﻿namespace MoviesWatchListAPI.Data
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesWatchListAPI.Models;
+
+namespace MoviesWatchListAPI.Data
 {
-    public class AppDbContext
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
+        public DbSet<Movie> Movies => Set<Movie>();
+        public DbSet<User> Users => Set<User>();
+
+        public DbSet<UserMovie> UserMovies => Set<UserMovie>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserMovie>()
+                .HasKey(um => new { um.UserId, um.MovieId });
+
+            modelBuilder.Entity<UserMovie>()
+                .HasOne(um => um.User)
+                .WithMany()
+                .HasForeignKey(um => um.UserId);
+
+            modelBuilder.Entity<UserMovie>()
+                .HasOne(um => um.Movie)
+                .WithMany()
+                .HasForeignKey(um => um.MovieId);
+        }
     }
 }
--- MoviesWatchListAPI/Program.cs
+++ MoviesWatchListAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<MoviesWatchListAPI.Data.AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'MoviesWatchListAPIContext' not found.")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
