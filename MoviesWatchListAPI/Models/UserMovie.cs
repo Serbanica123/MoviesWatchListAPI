f06ff29e8538360a936df69859552ee373b406c1
Nicolae Serban <nicolae.serban@techquarter.io>
2026-06-16 10:23:01 +0300

    Add project files.

---
+++ MoviesWatchListAPI/Models/UserMovie.cs
@@ -0,0 +1,15 @@
+﻿namespace MoviesWatchListAPI.Models
+{
+    public class UserMovie
+    {
+        public int UserId { get; set; }
+        public User? User { get; set; }
+
+        public int MovieId { get; set; }
+        public Movie? Movie { get; set; }
+
+        public bool Watched { get; set; }
+        public float? Rating { get; set; }
+        public DateTime AddedOn { get; set; }
+    }
+}
